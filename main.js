dc4037bb7052795499f7223439b70442942e66f0
ajayxverma <aajayaverma88927@gmail.com>
2023-03-13 15:33:15 +0530

    commit

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 
 const port = 3001;
-const routes = require("./routers/routes.js")
+const routes = require("./routers/routes.js");
 
 //console.log(routes)
 app.use(routes)
-
-
 
 app.listen(port, () => {
     console.log(`This app is listening at Port : ${port}`);
