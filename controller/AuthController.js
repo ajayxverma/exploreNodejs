94f17d3ee99d3e86f2f8f1d2e9dd5c5690654953
ajayxverma <aajayaverma88927@gmail.com>
2023-03-17 12:51:10 +0530

    17-03-2023

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,31 +1,38 @@
 const userAuth = require("../models/userModel");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 exports.singUp = async (req, res) => {
-
-    const newUser = await userAuth.create(req.body);
-
-    /* res.json({
+  const userPassWord = await bcrypt.hash(req.body.userPassWord, saltRounds);
+  const data = { ...req.body, userPassWord };
+  const newUser = await userAuth.create(data);
+  res.json({ newUser });
+  /* res.json({
         newUser: "User Created",
     }) */
-    res.json(newUser);
-}
+};
 
 exports.singIn = async (req, res) => {
-    
-    const findUser = await userAuth.findOne({userEmail:req.body.userEmail})
+  const findUser = await userAuth.findOne({ userEmail: req.body.userEmail });
 
-    if(!findUser) {
-        res.status(404).json({
-            findUser:"user Not found"
-        })
-    } else {
-        res.json(findUser);
-    }
+  if (!findUser) {
+    res.status(404).json({
+      findUser: "user Not found",
+    });
 
-    
-    /* res.json({
+    return;
+  }
+
+  if (!(await bcrypt.compare(req.body.userPassWord, findUser.userPassWord))) {
+    res.status(404).json({
+      findUser: "user Not found!, Incorrect Email or Password",
+    });
+
+    return;
+  }
+  res.json(findUser);
+
+  /* res.json({
         newUser: "User found",
     }) */
-}
-
+};
